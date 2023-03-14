90c9618979e76c876f9e4e5e141206dcc07d1efd
Mohammad matin <matin.ghiasvand1381@gmail.com>
2023-03-14 23:04:01 +0330

    Contract

---
+++ NEARCLI/deploy.bat
@@ -0,0 +1,2 @@
+near-cli.exe contract deploy c65ac8fafeab42c1d88a240faa29f1adb191f220be0715e45a08f3b99220d66f use-file status_message.wasm without-init-call network-config testnet sign-with-access-key-file Deployer.json
+del status_message.wasm
